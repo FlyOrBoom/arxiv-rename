db31bef4d28b4ec19d29fc6ada1ad89746a031bf
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2024-06-13 16:53:02 -0700

    Make it work

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # arxiv-rename
+
 Automatically renames downloaded arxiv PDFs to their author + year + title
+
+## Installation
+
+Clone this repository and install the required dependencies (`watchman` and `arxiv`).
+```bash
+git clone https://github.com/FlyOrBoom/arxiv-rename.git
+cd arxiv-rename
+pip install requirements.txt
+```
+
+## Usage
+
+Run this script in the background, 
+replacing `~/Downloads` with the path to the directory where your arxiv PDFs are downloaded to.
+```python
+python3 main.py ~/Downloads
+```
---
+++ main.py
@@ -0,0 +1,51 @@
+import sys
+import os
+import time
+import datetime
+import logging
+import re
+import arxiv
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+regex = re.compile(r"^(\d{4}\.\d{5}v\d+).*\.pdf$")
+client = arxiv.Client()
+
+class MyEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+
+        if event.is_directory: return
+
+        dirname = os.path.dirname(event.src_path)
+        basename = os.path.basename(event.src_path)
+
+        match = regex.match(basename)
+        if not match: return
+
+        paper_id = match.group(1)
+        paper = next(client.results(arxiv.Search(id_list=[paper_id])))
+
+        title = paper.title
+        updated = str(paper.updated)[:7]
+        author = paper.authors[0].name
+        if len(paper.authors) > 1: author += " et al"
+        newname = f"{author} {updated} - {title}.pdf"
+
+        os.rename(event.src_path, os.path.join(dirname, newname))
+        print(f"{paper_id} -> {newname}")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    logging.info(f'start watching directory {path!r}')
+    observer = Observer()
+    observer.schedule(MyEventHandler(), path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
